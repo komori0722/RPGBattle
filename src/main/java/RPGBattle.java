233e36be43dbb9ab3402aab076f3ee0bc2c77fc2
Komori Seishin <komosei25@gmail.com>
2026-09-20 22:55:18 +0900

    Merge branch 'master' of https://github.com/komori0722/RPGBattle

--- src/main/java/RPGBattle.java
+++ src/main/java/RPGBattle.java
@@ -7,20 +7,18 @@ public class RPGBattle { // ファイル名（RPGBattle.java）と合わせる
         double playerHP = 100;
         int playerMP = 50;
         int playerAttack_Power = 10;
-        double playerdamagerate = 0;
-        double playrtArrackdamege = 0;
-        int playerRecoveryamount = 0;
+        double playrtArrackdamege;
+        int playerRecoveryamount;
 
         //ボスのステータス
         double bossHP = 150;
         int bossAttack_Power = 10;
-        double bossdamegerate = 0;
-        double bossAttack_damege = 0;
+        double bossAttack_damege;
 
         //システム本体
         System.out.println("【ボスが現れた！】" +
                 "\n賢く立ち回って敵を倒そう！");
-        while (playerHP > 0 && bossHP > 0){
+        while (playerHP > 0 && bossHP > 0) {
 
             System.out.println("\n--------------------------------");
             System.out.println("【勇者】HP: " + playerHP + " | MP: " + playerMP);
@@ -31,52 +29,77 @@ public class RPGBattle { // ファイル名（RPGBattle.java）と合わせる
             int command = scanner.nextInt();
 
             //プレイヤーのターン
-            if (command == 1){
-                //ダメージ倍率の計算
-                playerdamagerate = ((int)(Math.random()* 11)) / 10.0 + 0.5;
-                playrtArrackdamege = playerAttack_Power * playerdamagerate;
-                //ダメージを与える処理
-                bossHP -= playrtArrackdamege;
-                System.out.println("(倍率" + playerdamagerate + "倍)");
-                System.out.println("ボスに" + playrtArrackdamege + "ダメージ入った！");
+            switch (command) {
+                case 1:
+                    // ★自作メソッドを呼び出してダメージを計算！
+                    playrtArrackdamege = calculateDamage(playerAttack_Power);
+                    //ダメージを与える処理
+                    bossHP -= playrtArrackdamege;
+                    System.out.println("ボスに" + playrtArrackdamege + "ダメージ入った！");
+                    break;
 
-            } else if (command == 2) {
-                if (playerMP >= 5) {
-                    playerMP -= 5;
-                    //回復率の計算
-                    playerRecoveryamount = (int) (Math.random() * 23) + 8;
-                    //回復を与える処理
-                    playerHP += playerRecoveryamount;
-                    System.out.println(playerRecoveryamount + "回復しました！");
-                } else {
-                    System.out.println("MPが足りない！");
+                case 2:
+                    if (playerMP >= 5) {
+                        playerMP -= 5;
+                        //回復率の計算
+                        playerRecoveryamount = (int) (Math.random() * 23) + 8;
+                        //回復を与える処理
+                        playerHP += playerRecoveryamount;
+                        System.out.println(playerRecoveryamount + "回復しました！");
+                    } else {
+                        System.out.println("MPが足りない！");
+                        continue;
+                    }
+                    break;
+                case 3:
+                    System.out.println("脱出した。にげてるようじゃ無理か（笑）ボスにはね、勝てないと");
+                    break;
+                default:
+                    System.out.println("正しいコマンドを入力してください。");
                     continue;
-                }
-            }else if (command == 3){
-                System.out.println("脱出した。にげてるようじゃ無理か（笑）ボスにはね、勝てないと");
-                break;
-            }else {
-                System.out.println("正しいコマンドを入力してください。");
-                continue;
             }
 
             //ボスのターン
-            if (bossHP > 0){
-                bossdamegerate = ((int)(Math.random()* 11)) / 10.0 + 0.5;
-                bossAttack_damege= bossAttack_Power * bossdamegerate;
-                playerHP -= bossAttack_damege;
+            if (bossHP > 0) {
+                // ★ボスも同じ自作メソッドを使いまわせる！
                 System.out.println("\n【ボスの反撃！】");
-                System.out.println("(倍率" + bossdamegerate + "倍)");
+                bossAttack_damege = calculateDamage(bossAttack_Power);// 変数 = メゾット名(メゾットに渡す変数)
+                playerHP -= bossAttack_damege;
                 System.out.println(bossAttack_damege + "のダメージをくらった！");
             }
 
         }
         //勝敗判定
         System.out.println("\n===========================");
-        if (bossHP <= 0){
+        if (bossHP <= 0) {
             System.out.println("魔王を倒した！世界に平和が訪れた！");
-        } else if (playerHP <= 0){
+        } else if (playerHP <= 0) {
             System.out.println("町を救う事はできなかった。GAMEOVER");
         }
+    }
+
+
+    // メソッド
+    // 攻撃力を引数(attackPower)で受け取り、ランダム倍率を掛けたダメージ(double)を返す
+    // =========================================================
+    public static double calculateDamage(int attackPower) {
+        //上のコードの解説public どこから呼び出してOK static mainメゾットから直接使える。
+        //double どんな完成品を返すか　少数の入った数字を返す
+        //calculateDamage メゾットの名前
+        //int attackPower 整数でデータを受け取って、そのメゾット内ではattackPowerという名前で扱います！という意味
+        double criticalRoll = Math.random();
+        double rate;
+
+        //20％の確率でクリティカル
+        if (criticalRoll < 0.2){
+            System.out.println("\n会心！\n");
+            rate = 2.0;
+        } else {
+            rate = ((int) (Math.random() * 11)) / 10.0 + 0.5;
+        }
+        double damage = attackPower * rate;
+        System.out.println("(倍率" + rate + "倍)");
+
+        return damage; // 結果を呼び出したところへ返す処理。returnがないと、計算されただけで、反映されない。
     }
 }
